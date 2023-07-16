f9e0cfbe991333978b9279c5fa37886193138ac0
Kevin Vega <kevinvega2070@gmail.com>
2023-07-15 22:40:39 -0300

    feat: notifications added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export * as elementsGenerators from './elementsGenerators';
 export * as nodeModifiers from './nodeModifiers';
+export * as notifications from './notifications';
 export * as selectors from './selectors';
+export * as timers from './timers';
--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.png/,
+        test: /\.(png|ico)/,
         type: 'asset/resource'
       },
       {
